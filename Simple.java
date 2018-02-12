7bf76992adfa2b7336baf1ae5f488c3db4e874a6
vijay shelkar <shelkarvijay94@gmail.com>
2018-02-12 13:13:47 +0530

    Add Inheritance

--- Simple.java
+++ Simple.java
@@ -2,10 +2,20 @@
         public static void main(String args[]){  
          System.out.println("Hello Java");
          Other o = new Other();
-        	o.fun();  
+        	o.fun();
+        	First f = new First();
+	        f.fun();
+  
         }  
     }  
     class Other{
+    	void fun(){
+    		System.out.println("I'm working for digi360i");
+    	}
+    }
+    //inheritance 
+
+    class First extends Other{
     	void fun(){
     		System.out.println("I'm working for digi360i");
     	}
