3a0cf4a0500abf78b750037ba454253eef4cab63
vijay shelkar <shelkarvijay94@gmail.com>
2018-02-12 12:54:27 +0530

    Hello word with function

--- Simple.java
+++ Simple.java
@@ -1,5 +1,12 @@
     class Simple{  
         public static void main(String args[]){  
-         System.out.println("Hello Java");  
+         System.out.println("Hello Java");
+         Other o = new Other();
+        	o.fun();  
         }  
     }  
+    class Other{
+    	void fun(){
+    		System.out.println("I'm working for digi360i");
+    	}
+    }
